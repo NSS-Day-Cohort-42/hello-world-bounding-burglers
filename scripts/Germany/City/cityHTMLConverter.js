8cfc2c39dc200705c4677554d1a8f1b9090d34c9
TylerSerafin <Tyler.Serafin2@gmail.com>
2020-07-21 09:21:26 -0500

    Added pictures and titles for landmarks, cities and heads of state

--- scripts/Germany/City/cityHTMLConverter.js
+++ scripts/Germany/City/cityHTMLConverter.js
@@ -1,7 +1,7 @@
 export const cityHTML = (cityObj) => {
       return `<section class="city card">
       <div class="city__title">${cityObj.title}</div>
-      <div><img class="city__image image--card" src="${cityObj.image}"/></div>
+      <div><img class="city__image image--card" src="${cityObj.image}"/></div>
       <div class="city__description">${cityObj.description}</div>
     </section>`
     }
--- scripts/Germany/HeadsofState/headsDataProvider.js
+++ scripts/Germany/HeadsofState/headsDataProvider.js
@@ -1,21 +1,21 @@
 const headsCollection = [
 
     {
-title: "",
-image: "",
+title: "President Frank-Walter Steinmeier",
+image: "https://static.euronews.com/articles/stories/04/62/59/48/602x338_cmsv2_ceb18914-039b-5b25-9b5e-16564c8f6c3e-4625948.jpg",
 description: ""
     },
 
     {
-title: "",
-image: "",   
+title: "Chancellor Angela Merkel",
+image: "https://image.cnbcfm.com/api/v1/image/106455891-1584899967247gettyimages-1207965186.jpeg?v=1584900016&w=678&h=381",   
 description: ""
     },
 
     {
-title: "",
-description: "",
-image: ""
+title: "Vice Chancellor Olaf Scholz",
+image: "https://www.filmfestivalcottbus.de/media/k2/items/cache/a2d776612246d598c744792a62711a11_XL.jpg",
+description: ""
     }
 ]
 
